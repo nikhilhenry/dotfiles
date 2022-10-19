fd149d51a9640181ce1d292f60032bab0e8aced6
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-10-19 23:08:24 +0530

    install telescope and treesiter

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -4,13 +4,23 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
     -- gruvbox theme
     use 'morhetz/gruvbox'
     -- lualine
     use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    -- telescope
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    -- telescope
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
     }
 end)
