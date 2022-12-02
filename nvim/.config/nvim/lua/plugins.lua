7608a28bce88b26d6f16923137893e593c2627dd
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-12-03 00:14:01 +0530

    setup harpoon

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -15,8 +15,12 @@ return require('packer').startup(function(use)
     }
     -- telescope
     use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim', tag = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {
+        'ThePrimeagen/harpoon',
+        requires = { {'nvim-lua/plenary.nvim'} }
     }
     -- nvim-treesitter 
     use {
